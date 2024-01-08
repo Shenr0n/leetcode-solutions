5f6d90639d1bdc0137bebf961e3efbcffc6b15f7
Siddhesh Shinde <siddhesh.shinde15@siesgst.ac.in>
2024-01-08 00:36:34 -0500

    Update 0100-same-tree.go

--- go/0100-same-tree.go
+++ go/0100-same-tree.go
@@ -7,14 +7,14 @@
  * }
  */
 func isSameTree(p *TreeNode, q *TreeNode) bool {
-
-    if p == nil || q == nil {
-        return (p == q)
+    if p == nil && q == nil {
+        return true
     }
-
+    if p == nil || q == nil{
+        return false
+    }
     if p.Val != q.Val {
         return false
     }
-
-    return isSameTree(p.Left, q.Left) && isSameTree(q.Right, q.Right)
+    return (isSameTree(p.Left, q.Left)) && (isSameTree(p.Right, q.Right))
 }
