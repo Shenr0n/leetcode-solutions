4f4a59a33eb39eef15681fefb59af00d4d4b1dcc
Siddhesh Shinde <siddhesh.shinde15@siesgst.ac.in>
2024-01-27 15:35:52 -0500

    Create hr-lonelyInteger.py

---
+++ python/hr-lonelyInteger.py
@@ -0,0 +1,39 @@
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+#
+# Complete the 'lonelyinteger' function below.
+#
+# The function is expected to return an INTEGER.
+# The function accepts INTEGER_ARRAY a as parameter.
+#
+
+def lonelyinteger(a):
+    # Write your code here
+    numMap = {}
+    for num in a:
+        if num not in numMap:
+            numMap[num] = 1
+        else:
+            numMap[num] += 1
+    for i, val in numMap.items():
+        if val == 1:
+            return i
+
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    n = int(input().strip())
+
+    a = list(map(int, input().rstrip().split()))
+
+    result = lonelyinteger(a)
+
+    fptr.write(str(result) + '\n')
+
+    fptr.close()
